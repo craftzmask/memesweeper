8953578a0e1bf1eb27392b5374123e7e3794e7df
Khanh Chung <khanh.d.chung@gmail.com>
2025-01-28 11:45:59 -0800

    setup for testing win condition

--- Engine/MemeField.h
+++ Engine/MemeField.h
@@ -42,8 +42,8 @@ public:
 private:
 	static constexpr Color borderColor = Colors::Blue;
 	static constexpr int borderWidth = 10;
-	static constexpr int width = 20;
-	static constexpr int height = 20;
+	static constexpr int width = 6;
+	static constexpr int height = 4;
 	Vei2 pos = { 400, 300 }; // center of the screen
 	Tile field[width * height];
 	bool isFucked = false;
